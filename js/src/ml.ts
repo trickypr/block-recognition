c280d3f58d77b62e9b6f45889c74ca764c88c480
trickypr <trickypr@icloud.com>
2021-09-15 21:09:50 +1000

    🎨  Cleaner code in the ML class

--- js/src/ml.ts
+++ js/src/ml.ts
@@ -1,29 +1,117 @@
 import * as tf from "@tensorflow/tfjs";
 import { load, MobileNet } from "@tensorflow-models/mobilenet";
-import { create } from "@tensorflow-models/knn-classifier";
+import { create, KNNClassifier } from "@tensorflow-models/knn-classifier";
 
 import catagories from "./public/contents.json";
-
-let net: MobileNet;
 
 const sleep = (ms: number) =>
   new Promise<void>((resolve) => setTimeout(resolve, ms));
 
-async function app() {
-  const modelOutput = document.getElementById("model");
+class FeedbackConsole {
+  private el: HTMLElement;
 
-  console.log("Loading mobilenet...");
-  modelOutput.innerHTML += "<br>Loading mobilenet...";
+  constructor(id: string) {
+    this.el = document.getElementById(id);
+  }
 
-  const netPromise = load();
-  const classifier = create();
-  net = await netPromise;
-  console.log("Successfully loaded model");
-  modelOutput.innerHTML += "<br>Mobilenet loaded";
+  append(text: string) {
+    this.el.innerHTML += text;
+    console.log(text);
+  }
 
-  console.log(catagories);
+  setRaw(text: string) {
+    this.el.innerHTML = `<pre>${text}</pre>`;
+    console.log(text);
+  }
 
-  async function addItem(classId: number, path: string) {
+  clear() {
+    this.el.innerHTML = "";
+  }
+}
+
+class Trainer {
+  private network: Promise<MobileNet>;
+  private classifier: KNNClassifier;
+
+  private classes = new Map();
+
+  private stateOutput = new FeedbackConsole("exampleOutput");
+  private modelOutput = new FeedbackConsole("model");
+
+  constructor() {
+    // Load mobilenet
+    this.network = load();
+
+    // Create the classifier
+    this.classifier = create();
+
+    this.stateOutput.append("Trainer ready");
+  }
+
+  async start() {
+    this.stateOutput.append("Loading mobilenet...");
+
+    // Wait for mobilenet to be done downloading
+    await this.network;
+
+    this.stateOutput.append("Mobilenet loaded");
+
+    // Load and train examples
+    await this.loadExamples();
+
+    // Output the model values
+    this.modelOutput.setRaw(
+      JSON.stringify(
+        Object.entries(this.classifier.getClassifierDataset()).map(
+          ([label, data]) => [label, Array.from(data.dataSync()), data.shape]
+        )
+      )
+    );
+
+    // Classify example
+    const testEl = document.getElementById("testingImage") as HTMLImageElement;
+    const testImg = tf.browser.fromPixels(testEl);
+
+    // Get the activation from mobilenet from the webcam.
+    const activation = (await this.network).infer(testImg, true);
+    // Get the most likely class and confidence from the classifier module.
+    const result = await this.classifier.predictClass(activation);
+
+    this.stateOutput.setRaw(
+      `Prediction:  ${this.classes.get(
+        result.label
+      )}\nProbability: ${this.formatConfidence(result.confidences)}`
+    );
+  }
+
+  private async loadExamples() {
+    const examples = [];
+
+    this.stateOutput.append("Loading examples...");
+
+    for (let classId = 0; classId < catagories.children.length; classId++) {
+      const thisClass = catagories.children[classId];
+
+      if (!thisClass.children || thisClass.name == "tests") {
+        continue;
+      }
+
+      this.classes.set(classId.toString(), thisClass.name);
+
+      for (const example of thisClass.children) {
+        examples.push(this.addExample(classId, example.path));
+        await sleep(10);
+      }
+    }
+
+    this.stateOutput.append("Examples loaded");
+
+    await Promise.all(examples);
+
+    this.stateOutput.append("Examples trained");
+  }
+
+  private async addExample(classId: number, path: string) {
     const url = path.replace("../src/", "/");
 
     const img = await new Promise<HTMLImageElement>((resolve, reject) => {
@@ -37,82 +125,37 @@ async function app() {
     });
 
     const tensor = tf.browser.fromPixels(img);
-    const activation = net.infer(tensor, true);
-    classifier.addExample(activation, classId.toString());
+    const activation = (await this.network).infer(tensor, true);
+    this.classifier.addExample(activation, classId.toString());
 
     img.remove();
     tensor.dispose();
   }
 
-  let classes = new Map();
-  const classExamples = [];
-
-  for (let classId = 0; classId < catagories.children.length; classId++) {
-    const thisClass = catagories.children[classId];
-
-    if (!thisClass.children || thisClass.name == "tests") {
-      continue;
-    }
-
-    classes.set(classId.toString(), thisClass.name);
-
-    for (const example of thisClass.children) {
-      classExamples.push(addItem(classId, example.path));
-    }
-  }
-
-  modelOutput.innerHTML += "<br>Downloading dataset and training...";
-  await Promise.all(classExamples);
-
-  function confidence(confidence: { [label: string]: number }): string {
+  private formatConfidence(confidence: { [label: string]: number }): string {
     let out = "\n";
 
     for (const label in confidence) {
-      out += `    ${classes.get(label)}: ${confidence[label]}\n`;
+      out += `    ${this.classes.get(label)}: ${confidence[label]}\n`;
     }
 
     return out;
   }
-
-  // Provide the model output to the user
-  modelOutput.innerHTML = `${JSON.stringify(
-    Object.entries(classifier.getClassifierDataset()).map(([label, data]) => [
-      label,
-      Array.from(data.dataSync()),
-      data.shape,
-    ])
-  )}`;
-
-  const testEl = document.getElementById("testingImage") as HTMLImageElement;
-  const testImg = tf.browser.fromPixels(testEl);
-
-  // Get the activation from mobilenet from the webcam.
-  const activation = net.infer(testImg, true);
-  // Get the most likely class and confidence from the classifier module.
-  const result = await classifier.predictClass(activation);
-
-  const exampleEl = document.getElementById("exampleOutput");
-
-  console.log(
-    `Prediction:  ${classes.get(result.label)}
-Probability: ${confidence(result.confidences)}`
-  );
-
-  exampleEl.innerHTML = `<pre>Prediction:  ${classes.get(result.label)}
-Probability: ${confidence(result.confidences)}</pre>`;
-
-  console.log(classifier.getClassifierDataset());
 }
+
+const trainer = new Trainer();
 
 const trainingButton = document.getElementById(
   "train_start"
 ) as HTMLButtonElement;
+
+// Wait for the user to click the button to start training
 trainingButton.addEventListener("click", async () => {
   trainingButton.disabled = true;
   trainingButton.innerText =
     "Training in progress. Please leave this tab in the foreground for around 5 minutes";
 
-  await app();
+  await trainer.start();
 
   trainingButton.style.display = "none";
 });
