62eff36ed1d6fc9d58957394dc1f8edcac75edb5
trickypr <trickypr@icloud.com>
2021-09-16 11:42:34 +1000

    ✨  Code for captuting images

--- robot/robot.js
+++ robot/robot.js
@@ -1,5 +1,10 @@
+// We want to use typescript typechecking in vscode, but don't want to have to
+// transpile
+// @ts-check
+
 // Assorted nodejs utilities for working with the file system etc
 const fs = require("fs");
+const { exec } = require("child_process") // Allows for the execution of bash commands
 
 // AI utilities
 const tf = require("@tensorflow/tfjs");
@@ -7,21 +12,130 @@ const mobileNet = require("@tensorflow-models/mobilenet");
 const knn = require("@tensorflow-models/knn-classifier");
 const tfnode = require("@tensorflow/tfjs-node");
 
+// Convert exec to a promise function
+const execPromise = cmd => new Promise((resolve, reject) => 
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      reject(err);
+    } else if (stderr) {
+      reject(stderr);
+    } else {
+      resolve(stdout);
+    }
+  }
+))
+
+/**
+ * I have gotten to used to rust's option for memory stafety, so I have written 
+ * a custom implementation
+ * 
+ * @template T
+ */
+class Option {
+  /**
+   * Creates an option instance
+   * @param {T?} value The value you want to store
+   */
+  constructor(value=undefined) {
+    this.set(value)
+  }
+
+  /**
+   * Returns the value or an error
+   * 
+   * @param {string} errorMessage The error message to print if this fails
+   * @returns {T}
+   */
+  unwrapOrError(errorMessage) {
+    if (this.hasValue) {
+      return this.value
+    } else {
+      throw new Error(errorMessage)
+    }
+  }
+
+  /**
+   * Return the value stored or the default value
+   * 
+   * @param {T} defaultValue The default value if this option is empty
+   * @returns {T} The value stored or the default value
+   */
+  unwrapOr(defaultValue) {
+    if (this.hasValue) {
+      return this.value
+    }
+
+    return defaultValue
+  }
+
+  /**
+   * Set a value in this option
+   * 
+   * @param {T?} value The value that should be stored
+   */
+  set(value=undefined) {
+    if (typeof value !== 'undefined') {
+      /**
+       * @private
+       */
+      this.value = value;
+
+      /**
+       * @private
+       */
+      this.hasValue = true;
+    } else {
+      this.hasValue = false;
+    }
+  }
+}
+
 // Assorted constants
 const CLASSIFIER_LOCATION = "path/to/classifier.json";
 
 // Store the cnn and classifier here
-let net = mobileNet.load();
+const netPromise = mobileNet.load();
+let net;
 let classifier = knn.create();
 
-function captureImage() {
-  // TODO: Make this capture and return an image path (requires hardware)
-  // Probably using raspi still https://www.raspberrypi.org/documentation/accessories/camera.html#raspistill
+/**
+ * Uses `raspistill` to capture an image from the webcam. This is non-blocking and
+ * may not return an output if there is an error
+ * 
+ * `raspistill` docs: https://www.raspberrypi.org/documentation/accessories/camera.html#raspistill
+ * 
+ * @returns {Promise<Option<string>>} An option for the path to the image
+ */
+async function captureImage() {
+  // If an error occurs, we will not return a value, so we want to use an option
+  const path = new Option();
+  
+  try {
+    const consoleOutput = await execPromise("raspistill -o currentBlock.jpg");
+    
+    // If there was an output from raspistill, output it to the console
+    if (consoleOutput && consoleOutput != "") {
+      console.log("raspistill:")
+      console.log(consoleOutput);
+    }
+
+    // Set the image path
+    path.set("currentBlock.jpg");
+  } catch (err) {
+    // If there was an error, place it here
+
+    console.error("Raspistill triggered an error.")
+    console.log(err);
+  }
+
+  return path
 }
 
 /**
  * This function will be responsible for classifying the image. It is asynchronous
  * to allow the robot to do other stuff (e.g. move the belt) in the background
+ * 
+ * @param {string} imagePath An option for the path to the image
  */
 async function classify(imagePath) {
   // Load the image from the local file system and convert it to a tensor (nodejs only)
@@ -53,7 +167,7 @@ async function main() {
   process.stdout.write("Loading mobilenet... ");
 
   // Wait for mobilenet to load before continuing
-  net = await net;
+  net = await netPromise;
   process.stdout.write("Done\n");
   process.stdout.write("Loading classifier... ");
 
@@ -72,10 +186,10 @@ async function main() {
 
   while (true) {
     // Capture an image from the robot's camera
-    const image = captureImage();
+    const image = await captureImage();
 
     // Start the image classifier the image
-    const classPromise = classify(image);
+    const classPromise = classify(image.unwrapOrError("No image captured"));
 
     // Increment the belt
     await incrementBelt();
