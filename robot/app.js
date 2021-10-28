ef1082f4ef93e4fbaa2b341533ab189125cd7125
trickypr <trickypr@icloud.com>
2021-10-28 12:09:13 +1100

    ✨ Add classifier mode

---
+++ robot/app.js
@@ -0,0 +1,32 @@
+const { Command } = require("commander");
+const fs = require("fs");
+
+const createServer = require("./server");
+const { execPromise } = require("./utils");
+
+const program = new Command();
+
+program.description("Image classifier that runs on a raspberry pi");
+
+program
+  .command("run")
+  .description(
+    "Starts the primary robot process and web server. Requires an external computer running firefox to perform image classification"
+  )
+  .action(() => createServer());
+
+program
+  .command("data <category>")
+  .description("Captures data to train the robot on")
+  .action((category) => {
+    fs.mkdirSync(`./data/${category}`, { recursive: true });
+    let i = 0;
+
+    while (true) {
+      await execPromise(
+        `raspistill -n -o data/${category}/${i}.jpg --width 1000 --height 1000 --timeout 0`
+      );
+
+      i++;
+    }
+  });
--- robot/server.js
+++ robot/server.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const server = http.createServer(app);
 
 const { Server } = require("socket.io");
-const io = new Server(server);
 
 const ml = require("./ml");
 
 const isOnPi = true;
 
-app.use(express.static("public"));
+function setupServer() {
+  const server = http.createServer(app);
+  const io = new Server(server);
 
-io.on("connection", (socket) => {
-  console.log("Front end has started");
+  app.use(express.static("public"));
 
-  if (isOnPi) {
-    ml(socket);
-  }
-});
+  io.on("connection", (socket) => {
+    console.log("Front end has started");
 
-server.listen(3000, () =>
-  console.log("Please open the web front end on a separate computer at *:3000")
-);
+    if (isOnPi) {
+      ml(socket);
+    }
+  });
+
+  server.listen(3000, () =>
+    console.log(
+      "Please open the web front end on a separate computer at *:3000"
+    )
+  );
+}
+
+module.exports = setupServer;
